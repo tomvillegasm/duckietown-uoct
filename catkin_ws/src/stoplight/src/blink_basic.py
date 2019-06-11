6d047f67cceb45a75545ffd47774d821e222c5b8
tomvillegasm <49348191+tomvillegasm@users.noreply.github.com>
2019-06-11 15:00:54 -0400

    Se añaden comentarios

--- catkin_ws/src/stoplight/src/blink_basic.py
+++ catkin_ws/src/stoplight/src/blink_basic.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
+#Indica a ROS ejecutar codigo con python
 
 import rospy
+#Importa los comandos de ROS
 from std_msgs.msg import Empty, String
+#Importa los tipos de dato a intercambiar (nada)
 from time import sleep
+#Para hacer delay
 
 rospy.init_node('blink_basic')
+#Inicia el nodo y le pone nombre
 pub = rospy.Publisher('/duckietown-uoct/stoplight/blink', Empty, queue_size=7)
+#Estructura del tipo (nombre-de-topico,tipo-de-dato,cantidad-instrucciones-acumuladas)
+#Aqui se crea el topico
 
 while not rospy.is_shutdown():
+#Establece como condicion ROS encendido    
     print 'Toggle'
     pub.publish()
-    # rospy.rate(10)
+    #Publica un dato (nada)
     sleep(1)
-# ros.rate()
-# rospy.spin()
