d060bf803bd462fc47a3604373c9790a3ee4085b
tomvillegasm <49348191+tomvillegasm@users.noreply.github.com>
2019-06-11 14:51:33 -0400

    Añade comentarios al código

--- catkin_ws/src/stoplight/src/Blink_basic.ino/Blink_basic.ino.ino
+++ catkin_ws/src/stoplight/src/Blink_basic.ino/Blink_basic.ino.ino
@@ -1,28 +1,33 @@
-/* 
- * rosserial Subscriber Example
- * Blinks an LED on callback
- */
-
 #include <ros.h>
+//Importa la librería ROS
 #include <std_msgs/Empty.h>
+//Establece tipo de datos a intercambiar (nada)
 
 ros::NodeHandle  nh;
+//Crea un nodo para ROS
 
-void messageCb( const std_msgs::Empty& toggle_msg){
-  digitalWrite(LED_BUILTIN, HIGH-digitalRead(LED_BUILTIN));   // blink the led
+void messageCb( const std_msgs::Empty& toggle_msg)
+{
+  digitalWrite(LED_BUILTIN, HIGH-digitalRead(LED_BUILTIN));
+  //La instrucción que el hardware realizará
 }
 
 ros::Subscriber<std_msgs::Empty> sub("/duckietown-uoct/stoplight/blink", &messageCb );
+//Establece el tópico al que se subscribirá y el tipo de datos a intercambiar (nada)
 
 void setup()
 { 
   pinMode(LED_BUILTIN, OUTPUT);
+  //El pin13 es configurado como una salida
   nh.initNode();
+  //Ejecuta el nodo
   nh.subscribe(sub);
+  //Se subscribe al tópico
 }
 
 void loop()
 {  
   nh.spinOnce();
+  //Mantiene el nodo activo y actualiza su estado
   delay(1);
 }
