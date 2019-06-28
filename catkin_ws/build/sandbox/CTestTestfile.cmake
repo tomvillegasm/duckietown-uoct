de57843541b5c543ccf93e7b7ef1ab5512783d82
tomvillegasm <tomas.villegas.m@ing.uchile.cl>
2019-06-27 22:56:39 -0400

    Actualización de cambios en ROS.

---
+++ catkin_ws/build/sandbox/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tomvillegasm/duckietown-uoct/catkin_ws/src/sandbox
+# Build directory: /home/tomvillegasm/duckietown-uoct/catkin_ws/build/sandbox
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
